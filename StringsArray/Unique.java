94938b76636cd0e901648cf1db9071735dc43a2a
nipoon777 <nipoon.donta7@gmail.com>
2021-01-25 11:59:17 +0530

    To check if characters are Unique

--- StringsArray/Unique.java
+++ StringsArray/Unique.java
@@ -2,11 +2,22 @@ package StringsArray;
 import java.util.*;
 
 public class Unique{
+    public static boolean isUnique( String str){
+        boolean[] charSet = new boolean[128];
+
+        for(int i = 0 ; i < str.length() ; i++){
+            int val = str.charAt(i);
+            if(charSet[val]){
+                return false;
+            }
+            charSet[val] = true;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Scanner scn = new Scanner( System.in);
         String str = scn.nextLine();
-
-        System.out.println("Hello " + str);
+        System.out.println(isUnique(str));
         scn.close();
     }
 
