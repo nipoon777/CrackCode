ccf51ef3a325d146962523b13a7a1d8a85d69990
nipoon777 <nipoon.donta7@gmail.com>
2021-01-25 11:50:54 +0530

    FirstCommit

---
+++ StringsArray/Unique.java
@@ -0,0 +1,13 @@
+package StringsArray;
+import java.util.*;
+
+public class Unique{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner( System.in);
+        String str = scn.nextLine();
+
+        System.out.println("Hello " + str);
+        scn.close();
+    }
+
+}
